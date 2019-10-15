3c72ec393dd4a28c40fb72badd7df981c2828aff
Sarah Murphy <44820813+sim39@users.noreply.github.com>
2019-10-15 12:27:46 -0400

    Update Performance_GN.R
    Made code more presentable

--- Performance_GN.R
+++ Performance_GN.R
@@ -4,15 +4,14 @@
 #Written by: Sarah I. Murphy
 #
 #Edited: October 15, 2019
-#
-#This analysis treats each individual sample as unique (regardless of shelf life day)
 ###########
 
 #load packages
 library(readr)
 library(dplyr)
+library(caret)
 
-##load data
+#load data
 df10 <- read_csv("raw_data/D10_VSL464.csv")
 df14 <- read_csv("raw_data/D14_VSL464.csv")
 df17 <- read_csv("raw_data/D17_VSL464.csv")
@@ -21,7 +20,7 @@ df21 <- read_csv("raw_data/D21_VSL464.csv")
 #join data
 df <- rbind(df10,df14,df17,df21)
 
-##adjust values so they are in 0,1 format where 0 is negative and 1 is positive
+#adjust values so they are in 0,1 format where 0 is negative and 1 is positive
 df[df=="--"] <- 0
 df[df=="-"] <- 0
 df[is.na(df)] <- 0
@@ -30,10 +29,10 @@ df[df=="-+"] <- 1
 df[df=="+-"] <- 1
 df[df=="+"] <- 1
 
+#clean up environment
 rm(df10,df14,df17,df21)
 
-
-#remove where all other tests are NT; this also effectively removed any row without CVTA
+#only include rows when data is available for all tests (note: "NT" indicates "not tested")
 dfc<-df[!(c(df$COLI_COLI_24 =="NT" & 
               df$COLI_NON_24 == "NT" &
               df$COLI_COLI_48 == "NT" &
@@ -43,14 +42,11 @@ dfc<-df[!(c(df$COLI_COLI_24 =="NT" &
               df$EB_COLI_48 == "NT" &
               df$EB_NON_48 == "NT")),]
 
-## convert all classes to factor
+#convert all classes to factor
 columns <- (colnames(dfc))
 dfc[columns] <- lapply(dfc[columns], factor) 
 
-## put the performance measures for each test in tables and save .csv
-library(caret)
-
-#
+#calculate performance measures for each test and put in a single dataframe
 cms_full <- NULL
 for( column in names(dfc[4:11]) ) {
   print(column)
@@ -64,4 +60,5 @@ for( column in names(dfc[4:11]) ) {
   cms_full <- rbind(cms,cms_full)
 }
 
-rm(cm,cms) #clean up environment
+#clean up environment
+rm(cm,cms) 
